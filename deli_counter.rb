c8bf3c79cd50318c46551530ae09460920651209
Dylan Michael Labat <dml252@live.com>
2019-01-21 13:51:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,6 @@ def now_serving(katz_deli)
   if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
